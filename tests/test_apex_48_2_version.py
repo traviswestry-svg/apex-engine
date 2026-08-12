a91b0c3cc472f36d544371496acb9f33c6a248bf
traviswestry-svg <traviswestry@gmail.com>
2026-08-12 17:43:24 -0400

    Add files via upload

--- tests/test_apex_48_2_version.py
+++ tests/test_apex_48_2_version.py
@@ -1,8 +1,11 @@
 from engine.release_manifest import RELEASE_MANIFEST
 from engine.release_manager import APPLICATION_VERSION, SEMANTIC_VERSION
+from engine.version import APPLICATION_VERSION as VERSION_MODULE_APPLICATION_VERSION
 
 
 def test_current_manifest_is_canonical_release():
-    assert RELEASE_MANIFEST["apex_version"] == "66.3.2"
-    assert APPLICATION_VERSION == "66.3.2"
-    assert SEMANTIC_VERSION == "66.3.2"
+    expected = RELEASE_MANIFEST["apex_version"]
+    assert expected
+    assert APPLICATION_VERSION == expected
+    assert SEMANTIC_VERSION == expected
+    assert VERSION_MODULE_APPLICATION_VERSION == expected
